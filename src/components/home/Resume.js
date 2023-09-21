48bcbd0c5758b7f466b40f471853093a45261fb0
Josh Waalkes <josh.waalkes21@gmail.com>
2023-09-21 15:31:17 -0500

    displaying resume

--- src/components/home/Resume.js
+++ src/components/home/Resume.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-export default function Resume () {
+export default function Resume() {
 
-  const style = {border: '2px solid blue'};
+  const style = { border: '2px solid blue' };
+  const currentYear = new Date().getFullYear();
+  // const currentYear = currentDate.getFullYear();
 
 
   return (
     <div style={style}>
       <h3>Resume</h3>
+      <iframe
+        className="resumeViewer"
+        src="../../../Joshua_Waalkes_Resume.pdf"
+        width="100%"
+        height="1200px"
+        title={currentYear + "Resume"}
+      />
       <button>Download as a PDF </button>
     </div>
   )
