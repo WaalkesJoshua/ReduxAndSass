b0629eac9f0d8cb5279a11c1c394f6c75dc0ea1a
Josh Waalkes <josh.waalkes21@gmail.com>
2023-09-22 13:58:39 -0500

    updated slicer, and resume, loading ok locally

--- src/components/home/Resume.js
+++ src/components/home/Resume.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setData, setDataURL, setError } from '../../slicers/resumeDataSlice';
 
 export default function Resume() {
+  const dispatch = useDispatch();
+  const pdfDataURL = useSelector((state) => state.resume.pdfDataURL);
+  const isError = useSelector((state) => state.resume.isError);
+  const googleDriveURL = useSelector((state) => state.resume.googleDriveURL);
+  const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
-  const style = { border: '2px solid blue' };
-  const currentYear = new Date().getFullYear();
-  // const currentYear = currentDate.getFullYear();
+  useEffect(() => {
+    fetch(corsProxyUrl + googleDriveURL)
+      .then((response) => response.blob())
+      .then((data) => {
+        const dataURL = URL.createObjectURL(data);
+        dispatch(setData(data));
+        dispatch(setDataURL(dataURL));
+      })
+      .catch((e) => {
+        console.error('Error fetching PDF file from: ' + googleDriveURL);
+        dispatch(setError(true))
+      });
+  }, []);
 
 
   return (
-    <div style={style}>
+    <div>
       <h3>Resume</h3>
+      //Ultimately I want to use react-pdf
+      {isError &&
+        <p>Error loading resume pdf</p>}
+      {(pdfDataURL && !isError) ? (
       <iframe
         className="resumeViewer"
-        src="/Joshua_Waalkes_Resume.pdf"
-        width="100%"
+        src={pdfDataURL}
+        width="75%"
         height="1200px"
-        title={currentYear + "Resume"}
+        title="Resume"
       />
-      <button>Download as a PDF </button>
+      ) : (
+        <p>Loading Resume ...</p>
+      )}
+      <button onClick={() => { alert("This button doesn't do anything yet!") }}> Download as a PDF </button>
     </div>
   )
 }
--- src/slicers/resumeDataSlice.js
+++ src/slicers/resumeDataSlice.js
@@ -3,15 +3,26 @@ import { createSlice } from '@Reduxjs/toolkit';
 export const resumeDataSlice = createSlice({
   name: 'resume',
   initialState: {
-    pdfFile: null
+    pdfData: null,
+    pdfDataURL: null,
+    googleDriveURL: "https://drive.google.com/file/d/1L2mk6Cn66SxLX8tsyAOiidCgOlUATtRN/view?usp=sharing",
+    isError: false
   },
   reducers: {
-    setFile: (state, {payload}) => {
-      state.pdfFile = payload;
+    setData: (state, { payload }) => {
+      state.pdfData = payload;
+    },
+    setDataURL: (state, { payload }) => {
+      state.pdfDataURL = payload;
+    },
+    setError: (state, { payload }) => {
+      if(payload === true || payload === false) {
+        state.isError = payload;
+      }
     }
   }
 
 })
 
-export const { setFile } = resumeDataSlice.actions;
+export const { setData, setDataURL, setError } = resumeDataSlice.actions;
 export default resumeDataSlice.reducer;
